a67bfefcc5e9fa534a9945d26914217b930ec364
nicolas-seto <nicolasseto@gmail.com>
2013-11-25 00:28:35 -0800

    Forgot an s in Games when making Foreign Key constraint

--- config/create.php
+++ config/create.php
@@ -31,11 +31,11 @@ if (mysqli_query($con,$query))
 mysqli_select_db($con, 'GameStore');
 
 // Create tables
-$create_games = "CREATE TABLE Games(Title VARCHAR(40) NOT NULL,GSerial INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,Price FLOAT(2,2) NOT NULL,updatedAt DATETIME);";
-$create_inventory = "CREATE TABLE Inventory(GSerial INT UNSIGNED NOT NULL PRIMARY KEY,InStock INT NOT NULL DEFAULT 0,NumSold INT NOT NULL DEFAULT 0,CONSTRAINT FOREIGN KEY(GSerial) REFERENCES Game(GSerial));";
-$create_gamedetails = "CREATE TABLE GameDetails(GSerial INT UNSIGNED NOT NULL PRIMARY KEY, Genre VARCHAR(10) NOT NULL, ESRBRating VARCHAR(3) NOT NULL, GameScore INT, Year YEAR NOT NULL, CONSTRAINT FOREIGN KEY(GSerial) REFERENCES Game(GSerial));";
-$create_customers = "CREATE TABLE Customers(FirstName VARCHAR(15) NOT NULL, MidInitial VARCHAR(1), LastName VARCHAR(15) NOT NULL, Email VARCHAR(50) NOT NULL PRIMARY KEY, Age INT NOT NULL, CustomerID INT UNSIGNED NOT NULL AUTO_INCREMENT, Gender CHAR(1), CHECK (Email NOT IN (SELECT Email in Customers))); ";
-$create_orders = "CREATE TABLE Orders(OrderID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, CustomerID INT UNSIGNED NOT NULL, GSerial INT UNSIGNED NOT NULL, NumBought INT NOT NULL, CHECK (CustomerID IN (SELECT CustomerID FROM Customers)), CHECK (NumBought <= (SELECT InStock IN Inventory WHERE Inventory.GSerial = GSerial)));";
+$create_games = "CREATE TABLE 'Games'('Title' VARCHAR(40) NOT NULL,'GSerial' INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,'Price' FLOAT(2,2) NOT NULL,'updatedAt' DATETIME);";
+$create_inventory = "CREATE TABLE 'Inventory'('GSerial' INT UNSIGNED NOT NULL PRIMARY KEY,'InStock' INT NOT NULL DEFAULT 0,'NumSold' INT NOT NULL DEFAULT 0,CONSTRAINT FOREIGN KEY('GSerial') REFERENCES 'Games'('GSerial'));";
+$create_gamedetails = "CREATE TABLE 'GameDetails'('GSerial' INT UNSIGNED NOT NULL PRIMARY KEY, 'Genre' VARCHAR(10) NOT NULL, 'ESRBRating' VARCHAR(3) NOT NULL, 'GameScore' INT, 'Year' YEAR NOT NULL, CONSTRAINT FOREIGN KEY('GSerial') REFERENCES 'Games'('GSerial'));";
+$create_customers = "CREATE TABLE 'Customers'('FirstName' VARCHAR(15) NOT NULL, 'MidInitial' VARCHAR(1), 'LastName' VARCHAR(15) NOT NULL, 'Email' VARCHAR(50) NOT NULL PRIMARY KEY, 'Age' INT NOT NULL, 'CustomerID' INT UNSIGNED NOT NULL AUTO_INCREMENT, 'Gender' CHAR(1), CHECK (Email NOT IN (SELECT Email in Customers))); ";
+$create_orders = "CREATE TABLE 'Orders'('OrderID' INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 'CustomerID' INT UNSIGNED NOT NULL, 'GSerial' INT UNSIGNED NOT NULL, 'NumBought' INT NOT NULL, CHECK (CustomerID IN (SELECT CustomerID FROM Customers)), CHECK (NumBought <= (SELECT InStock IN Inventory WHERE Inventory.GSerial = GSerial)));";
 // Execute multi_query
 if (mysqli_query($con, $create_games)) {
     echo "Successfully created table Games...\n";
